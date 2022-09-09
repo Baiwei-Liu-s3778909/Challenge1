12c654097f008cfec7103d2052b98efb8a67b2f8
Baiwei-Liu-s3778909 <s3778909@student.rmit.edu.au>
2022-09-09 20:26:46 +1000

    create the Dao

---
+++ src/main/java/com/s3778909/challenge1/dao/ItemDao.java
@@ -0,0 +1,20 @@
+package com.s3778909.challenge1.dao;
+
+
+import com.s3778909.challenge1.model.Item;
+import com.s3778909.challenge1.model.Items;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemDao
+{
+    private static Items list = new Items();
+
+    static
+    {
+        list.getItemList().add(new Item("1", "Bag", "good bag", "bag.jpg", 1390));
+        list.getItemList().add(new Item("2", "Computer", "good computer", "computer.jpg",2213));
+        list.getItemList().add(new Item("3", "Phone", "good phone", "titanic.jpg",2142));
+    }
+
+}
